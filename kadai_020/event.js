f818ce7cb4c3efba064f607b9daa6c98c8279ab9
keserasera-la <s86038091@gmail.com>
2026-08-17 18:16:03 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,5 +2,5 @@ const text = document.getElementById('text');
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
-  text.textContent = 'ボタンがクリックしました';
+  text.textContent = 'ボタンをクリックしました';
 });
